66f67bb94e121383ab65f3ccaea1b5ff2f216fc2
NikhilDevasia <nikhildevasia2000@gmail.com>
2023-09-08 15:02:05 +0530

    update train_pipeline.py
    Add ModelTrainer class with hyperparameter tuning and model selection logic

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,60 +1,115 @@
+import os
 import sys
-import pandas as pd
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+from xgboost import XGBRegressor
+
 from src.exception import CustomException
-from src.components.data_transformation import DataTransformationConfig
-from src.pipeline.train_pipeline import ModelTrainerConfig
-from src.utils import load_object
 from src.logger import logger
+from src.utils import save_object,evaluate_models
+from dataclasses import dataclass
 
 
-class PredictPipeline:
-    def __init__(self):
-        self.trained_model_path = ModelTrainerConfig()
-        self.preprocessor_file_path = DataTransformationConfig()
-
-    def predict(self, features):
-        try:
-            model_path = self.trained_model_path.trained_model_file_path
-            preprocessor_path = self.preprocessor_file_path.preprocessor_obj_file_path
-            model = load_object(file_path=model_path)
-            preprocessor = load_object(file_path=preprocessor_path)
-            data_scaled = preprocessor.transform(features)
-            logger.info("Before Starting the prediction")
-            return model.predict(data_scaled)
-        except Exception as e:
-            raise CustomException(e, sys) from e
-
-
-class CustomData:
-    def __init__(
-        self,
-        gender: str,
-        race_ethnicity: str,
-        parental_level_of_education,
-        lunch: str,
-        test_preparation_course: str,
-        reading_score: int,
-        writing_score: int):
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts",'model.pkl')
+    
+    
+class ModelTrainer:
+    
+    def __init__(self) -> None:
+        self.model_trainer_config=ModelTrainerConfig()
         
-        self.gender = gender
-        self.race_ethnicity = race_ethnicity
-        self.parental_level_of_education = parental_level_of_education
-        self.lunch = lunch
-        self.test_preparation_course = test_preparation_course
-        self.reading_score = reading_score
-        self.writing_score = writing_score
-
-    def get_data_as_data_frame(self) -> pd.DataFrame:
-        try:
-            custom_data_input_dict = {
-                "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
-                "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+    def get_params_for_hyper_tuning(self):
+        return {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbour Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    # 'weights':['uniform','distance'],
+                    # 'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    # 'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }   
             }
-            return pd.DataFrame(custom_data_input_dict)
+        
+    def initiate_model_train(self,train_array,test_array):
+        try:
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            logger.info("Splitted train and test data to X_train,y_train,X_test,y_test")
+            params = self.get_params_for_hyper_tuning()
+            # Regression models dict
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,params=params)
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+            ## To get best model name from dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+            # if model score doesn't score more than 60 percentage then the error will raise
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logger.info(f"Best found model :{best_model_name}")
+            # saving the model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(X_test)
+            return r2_score(y_test, predicted)    
         except Exception as e:
-            raise CustomException(e, sys) from e
+            raise CustomException(e,sys) from e
